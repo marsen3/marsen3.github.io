040cc7e4c289f00373a14e5a841ab430d2394992
marsen3 <35343352+marsen3@users.noreply.github.com>
2018-02-22 11:54:28 -0600

    5

---
+++ js/code-assignment05.js
@@ -0,0 +1,19 @@
+let projects = [{
+  id: 1,
+  url: 'http://arcg.is/2mK7lpE',
+  title: 'Book Assignment 01',
+  desc: 'Three points of interest pertaining to my life.',
+  thumb: true,
+  keywords: [New Orleans, Cities, Interest]
+  },
+
+
+  id: 2,
+  url: 'http://www.arcgis.com/apps/View/index.html?appid=56470d3e779e4e8bb6ad3a849ff7b382',
+  title: 'Book Assignment 02',
+  desc: 'World map is updated weekly with earthquakes.',
+  thumb: true,
+  keywords: ['Earthquakes', 'World Maps', 'Code Assignment4'] }
+
+for (let i=0; 1 <projects.length; i++){let x = i +1}
+console.log('Capture' + x + '.png');
