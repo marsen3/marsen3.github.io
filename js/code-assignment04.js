3387aadac738aa50705b68e2fe7bf87fc44dd264
marsen3 <35343352+marsen3@users.noreply.github.com>
2018-02-22 10:36:33 -0600

    r

--- js/code-assignment04.js
+++ js/code-assignment04.js
@@ -1,18 +1,18 @@
-let projectArray = [
+let projectArray1 = [
   1,
   'http://arcg.is/2mK7lpE',
   'Book Assignment 01',
   'Three points of interest pertaining to my life.',
   true ]
 
-let projectObject = {
+let projectObject1 = {
   id: 1,
   url: 'http://arcg.is/2mK7lpE',
   title: 'Book Assignment 01',
   desc: 'Three points of interest pertaining to my life.',
   thumb: true, }
 
-let projectArray = [
+let projectArray2 = [
   2,
   'http://www.arcgis.com/apps/View/index.html?appid=56470d3e779e4e8bb6ad3a849ff7b382',
   'Book Assignment 02',
@@ -20,7 +20,7 @@ let projectArray = [
   true,
   ['Book Assignment', 'Assignment', 'Code Assignment4'] ]
 
-let projectObject = {
+let projectObject2 = {
   id: 2,
   url: 'http://www.arcgis.com/apps/View/index.html?appid=56470d3e779e4e8bb6ad3a849ff7b382',
   title: 'Book Assignment 02',
