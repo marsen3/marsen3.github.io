1e3abba36c2273de33f001f9d1a07a754a5c7928
marsen3 <35343352+marsen3@users.noreply.github.com>
2018-02-27 10:53:32 -0600

    push

--- js/code-assignment05.js
+++ js/code-assignment05.js
@@ -1,13 +1,14 @@
-let projects = [{
+let projectData = []
+
+projectData[0] = {
   id: 1,
   url: 'http://arcg.is/2mK7lpE',
   title: 'Book Assignment 01',
   desc: 'Three points of interest pertaining to my life.',
   thumb: true,
-  keywords: [New Orleans, Cities, Interest]
-  }],
+  keywords: ['New Orleans', 'Cities', 'Interest']},
 
-
+projectData[1]
   id: 2,
   url: 'http://www.arcgis.com/apps/View/index.html?appid=56470d3e779e4e8bb6ad3a849ff7b382',
   title: 'Book Assignment 02',
@@ -15,5 +16,5 @@ let projects = [{
   thumb: true,
   keywords: ['Earthquakes', 'World Maps', 'Code Assignment4'] }
 
-for (let i=0; 1 <projects.length; i++){let x = i +1}
-console.log('Capture' + x + '.png');
+for (let i = 0; i <projectD.length; i++) {
+console.log('Capture' + projectData[i].id + '.png') }
