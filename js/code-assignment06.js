d73fd2b9fc47c91d8be363cc2ada2cb6d950349d
marsen3 <35343352+marsen3@users.noreply.github.com>
2018-03-20 11:15:11 -0500

    hello

--- js/code-assignment06.js
+++ js/code-assignment06.js
@@ -1,4 +1,5 @@
 for (let i = 0; i < projectData.length; i++) {
+  createTitle(projectData[i].title)
   if (projectData[i].thumb === true) {
     console.log('Capture' + projectData[i].id + '.png')
   } else {
@@ -7,7 +8,7 @@ for (let i = 0; i < projectData.length; i++) {
 }
 
 
-let createTitle = function () {
-  console.log('Capture' + projectData[i].id + '.png')
-} 
-let createThumbnail = function () {}
+function createTitle (title) {
+  console.log('Title' + title)
+}
+function createThumbnail () {}
