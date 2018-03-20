4abb4478361c01b55eb36127d1c3bf06ac285f7c
marsen3 <35343352+marsen3@users.noreply.github.com>
2018-03-20 11:34:01 -0500

    why me

---
+++ js/code-assignment07.js
@@ -0,0 +1,28 @@
+let projectData = []
+
+projectData[0] = {
+  id: 1,
+  url: 'http://arcg.is/2mK7lpE',
+  title: 'Book Assignment 01',
+  desc: 'Three points of interest pertaining to my life.',
+  thumb: true,
+  keywords: ['New Orleans', 'Cities', 'Interest']
+}
+
+for (let i = 0; i < projectData.length; i++) {
+  createTitle(projectData[i].title)
+  createThumbnail (projectData[i].thumb, projectData[i].id)
+}
+
+
+function createTitle (title) {
+  console.log('Title' + title)
+}
+
+function createThumbnail (thumb, id) {
+  if (thumb === true) {
+    console.log('Capture' + id + '.png')
+  } else {
+    console.log('no-preview.png')
+  }
+}
