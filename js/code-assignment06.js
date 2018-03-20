6fefee97ea515d80a2166e54eff1f1f06a42a270
marsen3 <35343352+marsen3@users.noreply.github.com>
2018-03-20 11:30:16 -0500

    yu

--- js/code-assignment06.js
+++ js/code-assignment06.js
@@ -1,3 +1,14 @@
+let projectData = []
+
+projectData[0] = {
+  id: 1,
+  url: 'http://arcg.is/2mK7lpE',
+  title: 'Book Assignment 01',
+  desc: 'Three points of interest pertaining to my life.',
+  thumb: true,
+  keywords: ['New Orleans', 'Cities', 'Interest']
+}
+
 for (let i = 0; i < projectData.length; i++) {
   createTitle(projectData[i].title)
   createThumbnail (projectData[i].thumb, projectData[i].id)
