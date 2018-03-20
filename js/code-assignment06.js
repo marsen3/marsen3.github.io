8dffa4597bea7e8de610645f83f4a0ef49201864
marsen3 <35343352+marsen3@users.noreply.github.com>
2018-03-20 11:12:07 -0500

    push

---
+++ js/code-assignment06.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < projectData.length; i++) {
+  if (projectData[i].thumb === true) {
+    console.log('Capture' + projectData[i].id + '.png')
+  } else {
+    console.log('no-preview.png')
+  }
+}
+
+
+let createTitle = function () {
+  console.log('Capture' + projectData[i].id + '.png')
+} 
+let createThumbnail = function () {}
