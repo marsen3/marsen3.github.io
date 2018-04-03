34177447358283f19be4a9ab4e4eac16e5e98597
marsen3 <35343352+marsen3@users.noreply.github.com>
2018-04-03 10:41:32 -0500

    r

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -8,6 +8,19 @@ L.tileLayer(basemap).addTo(MattMap)
 
 let marker = L.marker([29.993258,-90.098182]).addTo(MattMap)
 
+var pointA = new L.LatLng(29.963714, -90.038818);
+var pointB = new L.LatLng(29.967952, -90.013798);
+var pointList = [pointA, pointB];
+
+var firstpolyline = new L.Polyline(pointList, {
+    color: 'blue',
+    weight: 3,
+    opacity: 0.5,
+    smoothFactor: 1
+});
+firstpolyline.addTo(MattMap);
+
+
 let polygon = L.polygon([
   [29.991932, -90.085530],
   [29.977658, -90.086388],
