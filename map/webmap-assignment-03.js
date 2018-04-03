edc5616e061824a83e4b8843d9608433c60c49d4
marsen3 <35343352+marsen3@users.noreply.github.com>
2018-04-03 11:39:22 -0500

    yes

--- map/webmap-assignment-03.js
+++ map/webmap-assignment-03.js
@@ -8,10 +8,22 @@ L.tileLayer(basemap).addTo(MattMap)
 var greenIcon = L.icon({
     iconUrl: 'Music-Herald-Trumpet-icon.png',
     iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 L.marker([29.993258,-90.098182], {icon: greenIcon}).addTo(MattMap);
 L.marker([29.993258,-90.098182], {icon: greenIcon}).addTo(MattMap).bindPopup("This Trumpet Represents Jazz Music.");
+
+let myCoords = [
+  [29.991932, -90.085530],
+  [29.977658, -90.086388],
+  [29.984944, -90.071453]
+]
+
+
+let myStyle = {
+  color: 'black',
+  fillColor: 'green'
+}
+
+let mypolygon = L.polygon(myCoords, myStyle).addTo(MattMap)
